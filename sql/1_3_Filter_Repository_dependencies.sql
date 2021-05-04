92773ba9f0d393cb0dc3d164f2616133650b17b5
Pol Benats <pol.benats@unamur.be>
2021-05-04 10:00:12 +0200

    Updated notebook

--- sql/1_3_Filter_Repository_dependencies.sql
+++
@@ -1,6 +0,0 @@
---Temporary table containing database-dependent repositories and minimal meta-data to build SQL_NOSQL_REPOSITORY_DEPENDENCIES
---For DATASET_VERSION, use yyyyMMdd format in integer. i.e. 20200112
-INSERT INTO TEMP_FILTERED_REPOSITORY_DEPENDENCIES(REPOSITORY_NAME, DEPENDENCY_MANIFEST_PLATFORM, DEPENDENCY_MANIFEST_FILE, DEPENDENCY_NAME, DATASET_VERSION)
-    SELECT rep."Repository Name with Owner", rep."Manifest Platform", rep."Manifest Filepath", rep."Dependency Project Name", ?
-    FROM repository_dependencies rep
-    WHERE rep."Dependency Project Name" IN ( SELECT DISTINCT DEPENDENCY_NAME FROM SQL_NOSQL_DEPENDENCY );
--- sql/2_2_SQL_NOSQL_DATASET_REPOSITORIES.sql
+++
@@ -1,7 +0,0 @@
---For DATASET_VERSION, use yyyyMMdd format in integer. i.e. 20200112
-INSERT INTO SQL_NOSQL_DATASET_REPOSITORIES(DATASET_VERSION, REPOSITORY_NAME, REPOSITORY_LANGUAGE, HOST_TYPE, DESCRIPTION, STATUS, FORK, CREATION_DATETIME, LAST_UPDATE_DATETIME, LAST_PUSH_DATETIME, LAST_SYNC_DATETIME, HOMEPAGE_URL, SIZE, STARS_COUNT, FORKS_COUNT, OPEN_ISSUES_COUNT, WATCHERS_COUNT, CONTRIBUTORS_COUNT, SOURCE_RANK, SOURCE_FORK_REPOSITORY_NAME, LICENCE, KEYWORDS)
-SELECT ?, "Name with Owner", Language, "Host Type", Description, Status, Fork, "Created Timestamp", "Updated Timestamp", "Last pushed Timestamp", "Last Synced Timestamp", "Homepage URL", Size, "Stars Count", "Forks Count", "Open Issues Count", "Watchers Count", "Contributors Count", SourceRank, "Fork Source Name with Owner", License, Keywords
-FROM repositories
-WHERE "Name with Owner" IN (
-	SELECT DISTINCT REPOSITORY_NAME FROM SQL_NOSQL_REPOSITORY_DEPENDENCIES
-);
